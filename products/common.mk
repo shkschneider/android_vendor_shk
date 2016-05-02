a63090be5da829fadec9047794df5bc34489c0df
shkschneider <shk.schneider@gmail.com>
2016-05-02 14:51:33 +0200

    Prebuilt: add libjni_latinimegoogle for 32/64-bit devices

--- products/common.mk
+++ products/common.mk
@@ -20,7 +20,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.mod.name=ShkMod \
 	ro.mod.version=16.04.00 \
 	persist.adb.notify=0
-
 ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.adb.secure=1
@@ -28,12 +27,19 @@ endif
 
 PRODUCT_COPY_FILES += \
         vendor/shk/prebuilt/system/media/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_COPY_FILES += \
 	vendor/shk/prebuilt/system/media/audio/ringtones/Enter_the_Nexus.ogg:system/media/audio/ringtones/Enter_the_Nexus.ogg \
 	vendor/shk/prebuilt/system/media/audio/notifications/Teleport.ogg:system/media/audio/notifications/Teleport.ogg
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_COPY_FILES += \
+	vendor/shk/prebuilt/system/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+	vendor/shk/prebuilt/system/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/shk/overlay
 
 # EOF
