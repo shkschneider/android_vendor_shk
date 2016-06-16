e4f6b892a4d694a183670a8dad72e5b97a90bb4a
shkschneider <shk.schneider@gmail.com>
2016-06-16 15:44:44 +0200

    products: added angler and bullhead (not tested)

---
+++ products/shkmod_angler.mk
@@ -0,0 +1,30 @@
+#
+# Copyright 2016 ShkMod
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+$(call inherit-product, vendor/shk/products/common.mk)
+
+PRODUCT_NAME := shkmod_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	qemu.sf.lcd_density=518
+
+# EOF
