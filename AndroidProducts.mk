9f9d18808aac552fa053da1d4b15e3a6f01f796e
shkschneider <shk.schneider@gmail.com>
2016-06-28 11:37:53 +0200

    products: added all available products

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,6 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/products/shkmod_emulator.mk \
 	$(LOCAL_DIR)/products/shkmod_hammerhead.mk \
-	$(LOCAL_DIR)/products/shkmod_shamu.mk
+	$(LOCAL_DIR)/products/shkmod_shamu.mk \
+	$(LOCAL_DIR)/products/shkmod_bullhead.mk \
+	$(LOCAL_DIR)/products/shkmod_angler.mk
