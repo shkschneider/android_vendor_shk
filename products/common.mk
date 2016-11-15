37929716fdf843560b13395df3ca965322e2c2c0
shkschneider <shk.schneider@gmail.com>
2016-11-15 15:35:58 +0100

    Bump version

--- products/common.mk
+++ products/common.mk
@@ -18,7 +18,7 @@ PRODUCT_NAME ?= shkmod
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.mod.name=ShkMod \
-	ro.mod.version=16.11.10 \
+	ro.mod.version=16.11.15 \
 	persist.adb.notify=0
 ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PROPERTY_OVERRIDES += \
